742696233b59449331f7c13ae8095fc4c311d07c
jhonatanfigueiredo <cft.figueroa@gmail.com>
2022-01-03 18:33:08 -0300

    Methods of finding and displaying data.

---
+++ scripts/util.js
@@ -0,0 +1,22 @@
+function searchResult(zipCodeData) {
+
+    for (let data in zipCodeData) {
+        if (document.querySelector(`#${data}`)) {
+            document.querySelector(`#${data}`).value = zipCodeData[data];
+        }
+    }
+}
+
+let zipCodeData = async function (zipCodeNumber) {
+
+    let url = "https://api.zip-codes.com/ZipCodesAPI.svc/1.0/GetZipCodeDetails/" + zipCodeNumber + "?key=UTDR58K736VA3MBSE83S";
+    try {
+        let fetchData = await fetch(url);
+        let jsonData = await fetchData.json(); 
+        searchResult(jsonData.item);
+    } catch (error) {
+        alert(error);
+    }
+}
+
+export { zipCodeData };
